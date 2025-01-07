556ca74c5f311627e47c46fe0c23399080c12e13
Fathimath <faseela863gqqQq@@gmail.com>
2025-01-07 13:16:14 +0100

    add Contact component

--- components/Education/index.js
+++ components/Education/index.js
@@ -1,23 +1,25 @@
 export default function Education() {
   return (
-    <section>
+    <div>
       <h2>Bildungsweg</h2>
-      <h3>
-        Intensivprogramm aus Weiterbildung zum Web Developer- Neue Fische,
-        Hamburg 05/2024 – 08/2024
-      </h3>{" "}
-      Ein dreimonatiges Intensivprogramm bot eine fundierte Ausbildung in
-      wichtigen Frontend-Technologien und Entwicklungspraktiken. Der Lehrplan
-      legte den Schwerpunkt auf praxisnahe Projekte und Programmieraufgaben, um
-      die neu erworbenen Fähigkeiten praktisch anzuwenden und ein solides
-      Verständnis von branchenüblichen Tools zu erlangen. in 720
-      Unterrichtseinheiten folgende Inhalte gelernt, diskutiert und
-      unterschiedlichen Aufgaben und Projekten vertieft: ✓ Shell, Git, HTML5,
-      CSS, JavaScript, React, CSS in JS, Next.js, Node.js, MongoDB, VSCode ✓
-      Project: https://capstone-trail-blazer.vercel.app/
-      <h3>
-        Bachelor of Engineering in Computer Science- Kannur University- India
-      </h3>
-    </section>
+      <ul>
+        <li>
+          Intensivprogramm aus Weiterbildung zum Web Developer- Neue Fische,
+          Hamburg 05/2024 – 08/2024 Ein dreimonatiges Intensivprogramm bot eine
+          fundierte Ausbildung in wichtigen Frontend-Technologien und
+          Entwicklungspraktiken. Der Lehrplan legte den Schwerpunkt auf
+          praxisnahe Projekte und Programmieraufgaben, um die neu erworbenen
+          Fähigkeiten praktisch anzuwenden und ein solides Verständnis von
+          branchenüblichen Tools zu erlangen. in 720 Unterrichtseinheiten
+          folgende Inhalte gelernt, diskutiert und unterschiedlichen Aufgaben
+          und Projekten vertieft: ✓ Shell, Git, HTML5, CSS, JavaScript, React,
+          CSS in JS, Next.js, Node.js, MongoDB, VSCode ✓ Project:
+          https://capstone-trail-blazer.vercel.app/
+        </li>
+        <li>
+          Bachelor of Engineering in Computer Science- Kannur University- India
+        </li>
+      </ul>
+    </div>
   );
 }
