899462ec6299379df2eee7d2c5ee1037c2ae8b07
Fathimath <faseela863gqqQq@@gmail.com>
2025-01-07 10:46:19 +0100

    add component Education

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import About from "../components/About";
 import Header from "@/components/Header";
 import Certification from "@/components/Certification";
+import Education from "@/components/Education";
 export default function Home() {
   return (
     <>
       <Header />
       <About />
       <Certification />
+      <Education />
     </>
   );
 }
---
+++ components/Education/index.js
@@ -0,0 +1,23 @@
+export default function Education() {
+  return (
+    <section>
+      <h2>Bildungsweg</h2>
+      <h3>
+        Intensivprogramm aus Weiterbildung zum Web Developer- Neue Fische,
+        Hamburg 05/2024 – 08/2024
+      </h3>{" "}
+      Ein dreimonatiges Intensivprogramm bot eine fundierte Ausbildung in
+      wichtigen Frontend-Technologien und Entwicklungspraktiken. Der Lehrplan
+      legte den Schwerpunkt auf praxisnahe Projekte und Programmieraufgaben, um
+      die neu erworbenen Fähigkeiten praktisch anzuwenden und ein solides
+      Verständnis von branchenüblichen Tools zu erlangen. in 720
+      Unterrichtseinheiten folgende Inhalte gelernt, diskutiert und
+      unterschiedlichen Aufgaben und Projekten vertieft: ✓ Shell, Git, HTML5,
+      CSS, JavaScript, React, CSS in JS, Next.js, Node.js, MongoDB, VSCode ✓
+      Project: https://capstone-trail-blazer.vercel.app/
+      <h3>
+        Bachelor of Engineering in Computer Science- Kannur University- India
+      </h3>
+    </section>
+  );
+}
