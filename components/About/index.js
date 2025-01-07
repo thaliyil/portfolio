6e9dfcb6715e83fd1ce3768f3c536430b355e5b0
Fathimath <faseela863gqqQq@@gmail.com>
2025-01-07 10:10:49 +0100

    add component about

---
+++ components/About/index.js
@@ -0,0 +1,20 @@
+import style from "../About/about.css";
+
+export default function About() {
+  return (
+    <section>
+      <h2>über mich</h2>
+
+      <p>
+        Ich bin eine motivierte und fleißige Softwareentwicklerin, die stets auf
+        der Suche nach neuen Möglichkeiten ist, um meine Fähigkeiten einzusetzen
+        und weiter auszubauen. Neben meinem erfolgreichen Abschluss eines
+        Integrationskurses und eines Deutschberufssprachkurses auf C1- Niveau
+        habe ich durch ein intensives Weiterbildungsprogramm umfangreiche
+        Kenntnisse im Bereich der Webentwicklung erworben. Mit einer starken
+        Lernbereitschaft und dem Drang, innovative Lösungen zu entwickeln,
+        möchte ich in einem dynamischen Team an spannenden Projekten mitarbeiten
+      </p>
+    </section>
+  );
+}
---
+++ components/Header/index.js
@@ -0,0 +1,31 @@
+import style from "../Header/header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <ul>
+        <li>
+          <a href="#profile">Profile</a>
+        </li>
+        <li>
+          <a href="#education">Education</a>
+        </li>
+        <li>
+          <a href="#certifications">Certifications</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/your-github-username"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
